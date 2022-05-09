1ed0ffe78739021a0533c9a583b6f3fe11245aea
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2022-05-09 11:59:31 +0300

    excel

---
+++ CreateExcelFile/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CreateExcelFile
+{
+
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+
+}
---
+++ Excel/PersonModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Excel
+{
+    partial class Program
+    {
+        public class PersonModel
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        
+        }
+    }
+}
