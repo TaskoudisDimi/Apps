3006dd10b27b7bb7309c84df46da48995d4a18d0
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2022-06-06 17:32:58 +0300

    TCP

--- Library/Form6.cs
+++ Library/Form6.cs
@@ -35,35 +35,35 @@ namespace Library
 
         private void exportButton_Click(object sender, EventArgs e)
         {
-            saveFileDialog.InitialDirectory = "C:";
-            saveFileDialog.Filter = "Excel File|*xlsx";
-            if (saveFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
-                ExcelApp.Application.Workbooks.Add(Type.Missing);
-                ExcelApp.Columns.ColumnWidth = 20;
+            //saveFileDialog.InitialDirectory = "C:";
+            //saveFileDialog.Filter = "Excel File|*xlsx";
+            //if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+            //    ExcelApp.Application.Workbooks.Add(Type.Missing);
+            //    ExcelApp.Columns.ColumnWidth = 20;
 
-                for (int i = 1; i < exportDataGridView.Columns.Count; i++)
-                {
-                    ExcelApp.Cells[1, i] = exportDataGridView.Columns[i - 1].HeaderText;
+            //    for (int i = 1; i < exportDataGridView.Columns.Count; i++)
+            //    {
+            //        ExcelApp.Cells[1, i] = exportDataGridView.Columns[i - 1].HeaderText;
 
-                }
+            //    }
 
-                for (int i = 0; i < exportDataGridView.Columns.Count; i++)
-                {
-                    for(int j = 0; j < exportDataGridView.Columns.Count; j++)
-                    {
-                        ExcelApp.Cells[i + 2, j + 1] = exportDataGridView.Rows[i].Cells[j].Value.ToString();
-                    }
-                }
+            //    for (int i = 0; i < exportDataGridView.Columns.Count; i++)
+            //    {
+            //        for(int j = 0; j < exportDataGridView.Columns.Count; j++)
+            //        {
+            //            ExcelApp.Cells[i + 2, j + 1] = exportDataGridView.Rows[i].Cells[j].Value.ToString();
+            //        }
+            //    }
 
-                ExcelApp.ActiveWorkbook.SaveCopyAs(saveFileDialog.FileName.ToString());
-                ExcelApp.ActiveWorkbook.Saved = true;
-                ExcelApp.Quit();
+            //    ExcelApp.ActiveWorkbook.SaveCopyAs(saveFileDialog.FileName.ToString());
+            //    ExcelApp.ActiveWorkbook.Saved = true;
+            //    ExcelApp.Quit();
 
 
 
-            }
+            //}
         }
 
 
