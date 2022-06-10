e85f14221cba5fee764b84aaceed90da28a406a7
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2022-06-10 12:15:10 +0300

    GPSMap

---
+++ GPSMap/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace GPSMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.mapWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.streetTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.cityTextBox = new System.Windows.Forms.TextBox();
+            this.stateTextBox = new System.Windows.Forms.TextBox();
+            this.zipTextBox = new System.Windows.Forms.TextBox();
+            this.streetLabel = new System.Windows.Forms.Label();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.zipLabel = new System.Windows.Forms.Label();
+            this.mapLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.mapLabel);
+            this.panel1.Controls.Add(this.zipLabel);
+            this.panel1.Controls.Add(this.stateLabel);
+            this.panel1.Controls.Add(this.cityLabel);
+            this.panel1.Controls.Add(this.streetLabel);
+            this.panel1.Controls.Add(this.zipTextBox);
+            this.panel1.Controls.Add(this.stateTextBox);
+            this.panel1.Controls.Add(this.cityTextBox);
+            this.panel1.Controls.Add(this.searchButton);
+            this.panel1.Controls.Add(this.streetTextBox);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.mapWebBrowser);
+            this.panel2.Location = new System.Drawing.Point(233, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(555, 426);
+            this.panel2.TabIndex = 1;
+            // 
+            // mapWebBrowser
+            // 
+            this.mapWebBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapWebBrowser.Location = new System.Drawing.Point(0, 0);
+            this.mapWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.mapWebBrowser.Name = "mapWebBrowser";
+            this.mapWebBrowser.Size = new System.Drawing.Size(555, 426);
+            this.mapWebBrowser.TabIndex = 0;
+            // 
+            // streetTextBox
+            // 
+            this.streetTextBox.Location = new System.Drawing.Point(33, 93);
+            this.streetTextBox.Name = "streetTextBox";
+            this.streetTextBox.Size = new System.Drawing.Size(100, 20);
+            this.streetTextBox.TabIndex = 0;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(46, 351);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // cityTextBox
+            // 
+            this.cityTextBox.Location = new System.Drawing.Point(33, 147);
+            this.cityTextBox.Name = "cityTextBox";
+            this.cityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cityTextBox.TabIndex = 2;
+            // 
+            // stateTextBox
+            // 
+            this.stateTextBox.Location = new System.Drawing.Point(33, 199);
+            this.stateTextBox.Name = "stateTextBox";
+            this.stateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.stateTextBox.TabIndex = 3;
+            // 
+            // zipTextBox
+            // 
+            this.zipTextBox.Location = new System.Drawing.Point(33, 249);
+            this.zipTextBox.Name = "zipTextBox";
+            this.zipTextBox.Size = new System.Drawing.Size(100, 20);
+            this.zipTextBox.TabIndex = 4;
+            // 
+            // streetLabel
+            // 
+            this.streetLabel.AutoSize = true;
+            this.streetLabel.Location = new System.Drawing.Point(43, 77);
+            this.streetLabel.Name = "streetLabel";
+            this.streetLabel.Size = new System.Drawing.Size(35, 13);
+            this.streetLabel.TabIndex = 5;
+            this.streetLabel.Text = "Street";
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Location = new System.Drawing.Point(43, 131);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(24, 13);
+            this.cityLabel.TabIndex = 6;
+            this.cityLabel.Text = "City";
+            // 
+            // stateLabel
+            // 
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Location = new System.Drawing.Point(43, 183);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(32, 13);
+            this.stateLabel.TabIndex = 7;
+            this.stateLabel.Text = "State";
+            // 
+            // zipLabel
+            // 
+            this.zipLabel.AutoSize = true;
+            this.zipLabel.Location = new System.Drawing.Point(35, 233);
+            this.zipLabel.Name = "zipLabel";
+            this.zipLabel.Size = new System.Drawing.Size(22, 13);
+            this.zipLabel.TabIndex = 8;
+            this.zipLabel.Text = "Zip";
+            // 
+            // mapLabel
+            // 
+            this.mapLabel.AutoSize = true;
+            this.mapLabel.Location = new System.Drawing.Point(13, 13);
+            this.mapLabel.Name = "mapLabel";
+            this.mapLabel.Size = new System.Drawing.Size(28, 13);
+            this.mapLabel.TabIndex = 9;
+            this.mapLabel.Text = "Map";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label streetLabel;
+        private System.Windows.Forms.TextBox zipTextBox;
+        private System.Windows.Forms.TextBox stateTextBox;
+        private System.Windows.Forms.TextBox cityTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox streetTextBox;
+        private System.Windows.Forms.WebBrowser mapWebBrowser;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Label zipLabel;
+        private System.Windows.Forms.Label mapLabel;
+    }
+}
+
---
+++ GPSMap/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPSMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            mapWebBrowser.ScriptErrorsSuppressed = true;
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string street = streetTextBox.Text;
+            string city = cityTextBox.Text;
+            string state = stateTextBox.Text;
+            string zip = zipTextBox.Text;
+
+            StringBuilder queryaddress = new StringBuilder();
+            queryaddress.Append("http://google.com/maps?q=");
+            if (street != string.Empty)
+            {
+                queryaddress.Append(street + "," + "+");
+            }
+            if(city != string.Empty)
+            {
+                queryaddress.Append(city + "," + "+");
+            }
+            if(state != string.Empty)
+            {
+                queryaddress.Append(state + "," + "+");
+            }
+            if(zip != string.Empty)
+            {
+                queryaddress.Append(zip + "," + "+");
+            }
+
+            mapWebBrowser.Navigate(queryaddress.ToString());
+
+
+        }
+    }
+}
