51cc61734aea717b8f49bc7fd680fcf8864ea156
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2022-06-20 17:09:34 +0300

    datetime

--- Library/Form6.Designer.cs
+++ Library/Form6.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.exportDataGridView = new System.Windows.Forms.DataGridView();
-            this.exportButton = new System.Windows.Forms.Button();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.exportDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,32 +38,29 @@
             // 
             this.exportDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.exportDataGridView.Location = new System.Drawing.Point(8, 84);
-            this.exportDataGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.exportDataGridView.Margin = new System.Windows.Forms.Padding(2);
             this.exportDataGridView.Name = "exportDataGridView";
             this.exportDataGridView.RowHeadersWidth = 62;
             this.exportDataGridView.RowTemplate.Height = 33;
-            this.exportDataGridView.Size = new System.Drawing.Size(821, 499);
+            this.exportDataGridView.Size = new System.Drawing.Size(821, 244);
             this.exportDataGridView.TabIndex = 0;
             // 
-            // exportButton
+            // dateTimePicker1
             // 
-            this.exportButton.Location = new System.Drawing.Point(734, 49);
-            this.exportButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.exportButton.Name = "exportButton";
-            this.exportButton.Size = new System.Drawing.Size(78, 20);
-            this.exportButton.TabIndex = 1;
-            this.exportButton.Text = "Export";
-            this.exportButton.UseVisualStyleBackColor = true;
-            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            this.dateTimePicker1.Location = new System.Drawing.Point(8, 28);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // Form6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(838, 606);
-            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.exportDataGridView);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form6";
             this.Text = "Form6";
             ((System.ComponentModel.ISupportInitialize)(this.exportDataGridView)).EndInit();
@@ -74,7 +71,7 @@
         #endregion
 
         private DataGridView exportDataGridView;
-        private Button exportButton;
         private SaveFileDialog saveFileDialog;
+        private DateTimePicker dateTimePicker1;
     }
 }
