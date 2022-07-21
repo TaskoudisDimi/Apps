1cee9695f67f3d69c0af230ef343e039a9423ee2
Taskoudis Dimitris <taskoudisdimitris@gmail.com>
2022-07-21 15:12:22 +0300

    Udemy Tutorial

---
+++ Udemy/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net;
+
+
+
+
+namespace Udemy
+{
+    public static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            int convertedInputNumber;
+            int.TryParse(input, out convertedInputNumber);
+
+            int number = 90;
+            int result = 10 + 1000 - number + convertedInputNumber;
+            Console.WriteLine(result);  
+
+
+
+        }
+    }
+}
