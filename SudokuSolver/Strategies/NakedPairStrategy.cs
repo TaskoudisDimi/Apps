8a406284b563668d1494e5ee46888cc7021c209f
Dimitrios Taskoudis <detaskou@office365.auth.gr>
2022-08-20 00:44:57 +0300

    End of Sudoku Solver

---
+++ SudokuSolver/Strategies/NakedPairStrategy.cs
@@ -0,0 +1,131 @@
+﻿using SudokuSolver.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Strategies
+{
+    class NakedPairsStrategy : ISudokuStrategy
+    {
+        private readonly SudokuMapper _sudokuMapper;
+
+        public NakedPairsStrategy(SudokuMapper sudokuMapper)
+        {
+            _sudokuMapper = sudokuMapper;
+        }
+
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    EliminateNakedPairFromOthersInRow(sudokuBoard, row, col);
+                    EliminateNakedPairFromOthersInCol(sudokuBoard, row, col);
+                    EliminateNakedPairFromOthersInBlock(sudokuBoard, row, col);
+                }
+            }
+
+            return sudokuBoard;
+        }
+
+        private void EliminateNakedPairFromOthersInRow(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            if (!HasNakedPairInRow(sudokuBoard, givenRow, givenCol)) return;
+
+            for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+            {
+                if (sudokuBoard[givenRow, col] != sudokuBoard[givenRow, givenCol] && sudokuBoard[givenRow, col].ToString().Length > 1)
+                {
+                    //Σε περίπτωση που έχω |9| || |12| || |12| |123|
+                    //Με το EliminateNakedPair διαγράφω το 12 από το 123 κελί
+                    EliminateNakedPair(sudokuBoard, sudokuBoard[givenRow, givenCol], givenRow, col);
+                }
+            }
+        }
+
+        private bool HasNakedPairInRow(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+            {
+                if (givenCol != col && IsNakedPair(sudokuBoard[givenRow, col], sudokuBoard[givenRow, givenCol])) return true;
+            }
+
+            return false;
+        }
+
+        private void EliminateNakedPairFromOthersInCol(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            if (!HasNakedPairInCol(sudokuBoard, givenRow, givenCol)) return;
+
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                if (sudokuBoard[row, givenCol] != sudokuBoard[givenRow, givenCol] && sudokuBoard[row, givenCol].ToString().Length > 1)
+                {
+                    EliminateNakedPair(sudokuBoard, sudokuBoard[givenRow, givenCol], row, givenCol);
+                }
+            }
+        }
+
+        private bool HasNakedPairInCol(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                if (givenRow != row && IsNakedPair(sudokuBoard[row, givenCol], sudokuBoard[givenRow, givenCol])) return true;
+            }
+
+            return false;
+        }
+
+        private void EliminateNakedPairFromOthersInBlock(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            if (!HasNakedPairInBlock(sudokuBoard, givenRow, givenCol)) return;
+
+            var sudokuMap = _sudokuMapper.Find(givenRow, givenCol);
+
+            for (int row = sudokuMap.SudokuRow; row <= sudokuMap.SudokuRow + 2; row++)
+            {
+                for (int col = sudokuMap.SudokuCol; col <= sudokuMap.SudokuCol + 2; col++)
+                {
+                    if (sudokuBoard[row, col].ToString().Length > 1 && sudokuBoard[row, col] != sudokuBoard[givenRow, givenCol])
+                    {
+                        EliminateNakedPair(sudokuBoard, sudokuBoard[givenRow, givenCol], row, col);
+                    }
+                }
+            }
+        }
+
+        private bool HasNakedPairInBlock(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    var elementSame = givenRow == row && givenCol == col;
+                    var elementInSameBlock = _sudokuMapper.Find(givenRow, givenCol).SudokuRow == _sudokuMapper.Find(row, col).SudokuRow &&
+                        _sudokuMapper.Find(givenRow, givenCol).SudokuCol == _sudokuMapper.Find(row, col).SudokuCol;
+
+                    if (!elementSame && elementInSameBlock && IsNakedPair(sudokuBoard[givenRow, givenCol], sudokuBoard[row, col])) return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void EliminateNakedPair(int[,] sudokuBoard, int valuesToEliminate, int eliminateFromRow, int eliminateFromCol)
+        {
+            var valuesToEliminateArray = valuesToEliminate.ToString().ToCharArray();
+            foreach (var valueToEliminate in valuesToEliminateArray)
+            {
+                sudokuBoard[eliminateFromRow, eliminateFromCol] = Convert.ToInt32(sudokuBoard[eliminateFromRow, eliminateFromCol].ToString().Replace(valueToEliminate.ToString(), string.Empty));
+            }
+        }
+
+        private bool IsNakedPair(int firstPair, int secondPair)
+        {
+            return firstPair.ToString().Length == 2 && firstPair == secondPair;
+        }
+    }
+}
--- SudokuSolver/Strategies/SimpleMarkUpStrategy.cs
+++ SudokuSolver/Strategies/SimpleMarkUpStrategy.cs
@@ -11,9 +11,85 @@ namespace SudokuSolver.Strategies
     {
         private readonly SudokuMapper _sudokuMapper;
 
-        public int[,] Solve(int[,] suodkuBoard)
+
+        public SimpleMarkUpStrategy(SudokuMapper sudokuMapper)
         {
-            throw new NotImplementedException();
+            _sudokuMapper = sudokuMapper;
+        }
+
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (var col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    if(sudokuBoard[row, col] == 0 || sudokuBoard[row, col].ToString().Length > 1)
+                    {
+                        var possibilitiesInRowAndCol = GetPossibilitiesInRowAndCol(sudokuBoard, row, col);
+                        var possibilitiesInBlock = GetPossibilitiesInBlock(sudokuBoard, row, col);
+                        sudokuBoard[row, col] = GetPossibilityIntersection(possibilitiesInRowAndCol, possibilitiesInBlock);
+
+                    }
+                }
+                
+            }
+            return sudokuBoard;
+        }
+
+        private int GetPossibilityIntersection(int possibilitiesInRowAndCol, int possibilitiesInBlock)
+        {
+            var possibilitiesInRowAndColCharArray = possibilitiesInRowAndCol.ToString().ToCharArray();
+            var possibilitiesInBlockCharArray = possibilitiesInBlock.ToString().ToCharArray();
+            var possibilitiesSubset = possibilitiesInRowAndColCharArray.Intersect(possibilitiesInBlockCharArray);
+            return Convert.ToInt32(string.Join(string.Empty, possibilitiesSubset));
+        }
+
+        private int GetPossibilitiesInBlock(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            int[] possibilities = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var sudokuMap = _sudokuMapper.Find(givenRow, givenCol);
+
+            for (int row = sudokuMap.SudokuRow; row <= sudokuMap.SudokuRow + 2; row++)
+            {
+                for (var col = sudokuMap.SudokuCol; col <= sudokuMap.SudokuCol + 2; col++)
+                {
+                    if (IsValidSingle(sudokuBoard[row, col]))
+                    {
+                        possibilities[sudokuBoard[row, col] - 1] = 0;
+                    }
+                }
+            }
+            return Convert.ToInt32(String.Join(String.Empty, possibilities.Select(x => x).Where(x => x != 0)));
+        }
+
+        private int GetPossibilitiesInRowAndCol(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            int[] possibilities = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            //Κάνω 0 το στοιχείο στον πίνακα πιθανοτητων σε περίπτωση που το κελί εχει τιμή
+            for (int col = 0; col < 0; col++)
+            {
+                if(IsValidSingle(sudokuBoard[givenRow, col]))
+                {
+                    possibilities[sudokuBoard[givenRow, col] - 1] = 0;  
+                }
+            }
+
+            for (int row = 0; row < 0; row++)
+            {
+                if (IsValidSingle(sudokuBoard[row, givenCol]))
+                {
+                    possibilities[sudokuBoard[row, givenCol] - 1] = 0;
+                }
+            }
+
+            //Επιστρέφει τις τιμές του πίνακα possibilities που δεν είναι μηδέν
+            return Convert.ToInt32(String.Join(string.Empty, possibilities.Select(x => x).Where(x => x != 0)));
+        }
+      
+        private bool IsValidSingle(int cellDigit)
+        {
+            return cellDigit != 0 && cellDigit.ToString().Length == 1;
         }
     }
 }
