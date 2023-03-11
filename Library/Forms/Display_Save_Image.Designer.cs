aa323b6f521084489855ad312ca3119a2a0dbac5
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-03-12 00:36:50 +0200

    save image to db

---
+++ Library/Forms/Display_Save_Image.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Forms.Forms
+{
+    partial class Display_Save_Image
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDisplayDB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(189, 49);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(370, 214);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(189, 374);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(112, 34);
+            this.button.TabIndex = 1;
+            this.button.Text = "Upload";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(325, 374);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(112, 34);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonDisplayDB
+            // 
+            this.buttonDisplayDB.Location = new System.Drawing.Point(447, 374);
+            this.buttonDisplayDB.Name = "buttonDisplayDB";
+            this.buttonDisplayDB.Size = new System.Drawing.Size(156, 34);
+            this.buttonDisplayDB.TabIndex = 3;
+            this.buttonDisplayDB.Text = "Display From DB";
+            this.buttonDisplayDB.UseVisualStyleBackColor = true;
+            this.buttonDisplayDB.Click += new System.EventHandler(this.buttonDisplayDB_Click);
+            // 
+            // Display_Save_Image
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDisplayDB);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Display_Save_Image";
+            this.Text = "Display_Save_Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox;
+        private Button button;
+        private Button buttonSave;
+        private Button buttonDisplayDB;
+    }
+}
---
+++ Library/Forms/Display_Save_Image.cs
@@ -0,0 +1,76 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Forms.Forms
+{
+    public partial class Display_Save_Image : Form
+    {
+        public static SqlConnection con = new SqlConnection();
+        string imageName = "";
+        public Display_Save_Image()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                imageName = ofd.FileName;
+            }
+        }
+
+
+        private void image()
+        {
+            byte[] imageData = File.ReadAllBytes(imageName);
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["Supermarket"].ConnectionString;
+            string connectionString = con.ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO TestImage (Image) VALUES (@ImageData)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.Add("@ImageData", SqlDbType.VarBinary, -1).Value = imageData;
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            image();
+        }
+
+        private void buttonDisplayDB_Click(object sender, EventArgs e)
+        {
+            //string connectionString = "your connection string";
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    connection.Open();
+            //    string query = "SELECT ImageData FROM Images WHERE Id = @Id";
+            //    SqlCommand command = new SqlCommand(query, connection);
+            //    command.Parameters.AddWithValue("@Id", imageId);
+            //    byte[] imageData = (byte[])command.ExecuteScalar();
+            //    using (MemoryStream stream = new MemoryStream(imageData))
+            //    {
+            //        pictureBox1.Image = Image.FromStream(stream);
+            //    }
+            //}
+
+        }
+    }
+}
