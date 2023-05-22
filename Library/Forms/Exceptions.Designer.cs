2eccc844c51c3c1a60142bc550475593dbdea501
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-05-22 23:18:36 +0300

    add exception to the sql server table

---
+++ Library/Forms/Exceptions.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Forms.Forms
+{
+    partial class Exceptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(79, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(874, 477);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(475, 567);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;           
+            // 
+            // Exceptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 625);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Exceptions";
+            this.Text = "Exceptions";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+    }
+}
---
+++ Library/Forms/Exceptions.cs
@@ -0,0 +1,43 @@
+﻿using com.itextpdf.text.pdf;
+using Library;
+using OfficeOpenXml.LoadFunctions.Params;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms.Forms
+{
+    public partial class Exceptions : Form
+    {
+        public Exceptions()
+        {
+            InitializeComponent();
+            Loaddata();
+        }
+
+        private void Loaddata()
+        {
+            try
+            {
+
+                dataGridView1.Rows[1].ToString();
+                //SqlConnect loaddata = new SqlConnect();
+                //loaddata.retrieveData("Select * From Exceptions");
+                //dataGridView1.DataSource = loaddata.table;
+            }
+            catch (Exception ex)
+            {
+                SqlConnect exc = new SqlConnect();
+                string exception = $"Insert Into Exceptions Values('{ex.Message.Replace("'","")}', '{ex.StackTrace}', '{DateTime.Now}')";
+                exc.commandExc(exception);
+            }
+        }
+
+    }
+}
