e5b15fc85e2b4995469253b8b3bb9909f60829bc
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-06-12 12:41:52 +0300

    Ciritical Sections

--- ParallelProgramming/Program.cs
+++ ParallelProgramming/Program.cs
@@ -7,6 +7,7 @@ namespace ParallelProgramming
     {
         static void Main(string[] args)
         {
+            #region TASK CLASS
             //TASK CLASS
             //string text1 = "Hello World Test 1";
             //string text2 = "Test 2";
@@ -107,42 +108,105 @@ namespace ParallelProgramming
             //Console.WriteLine($"Task t2 status is {t2.Status}");
 
             //EXCEPTION HANDLING
+            //var t = Task.Factory.StartNew(() =>
+            //{
+            //    throw new InvalidOperationException("Can't do this!") { Source = "t"};
+            //});
+            //var t2 = Task.Factory.StartNew(() =>
+            //{
+            //    throw new AccessViolationException("Can't access this!") { Source = "t2" };
+            //});
+            //try
+            //{
+            //    Task.WaitAll(t, t2);
+            //}
+            //catch (AggregateException ae)
+            //{
+            //    //foreach(var a in ae.InnerExceptions)
+            //    //{
+            //    //    Console.WriteLine($"Exception {a.GetType()} from {a.Source}");
+            //    //}
+            //    ae.Handle(e =>
+            //    {
+            //        if (e is InvalidOperationException)
+            //        {
+            //            Console.WriteLine("Invalid op!");
+            //            return true;
+            //        }
+            //        else return false;
+            //    });
+            //}
 
-            var t = Task.Factory.StartNew(() =>
+
+
+            //SUMMARY
+            //Create Task with 2 ways:
+            ////1)
+            //var t = new Task(() =>
+            //{
+            //    Console.WriteLine("Test");
+            //});
+            //t.Start();
+
+            ////2)
+            //Task.Factory.StartNew(() =>
+            //{
+            //    Console.WriteLine("Test");
+            //});
+
+            //Tasks can be passed an object
+            //Tasks can return values: new Task<T>, task.Result
+            //Tasks can report their State: task.IsCompleted, task.IsFaulted, etc.
+
+            //Cancellation
+            //Cancellation is supported via CancellationTokenSource, which returns a CancellationToken token = cts.Token
+            //The Token is passed into the function Task.Factory.StartNew(() => {},token);
+            //To cancel -> cts.Cancel()
+            //Task can check with token.IsCancellationRequested or token.ThrowIfCancellationRequested()
+
+            //Waiting for Time to Pass
+            //1) Thread.Sleep() 2)token.WaitHangle.WaitOne() -> return a bool indicating whether cancellation was requested in the time period specified
+            //Thread.SpinWait()
+            //SpinWait.SpinUntil(()=>{}) Spin waiting does not give up the thread's turn
+            //Waiting for Single task
+            //task.Wiat(optional timeout)
+            //Waiting for several tasks
+            //Task.WaitAll(t, t2)
+            //Task.WaitAAny(t, t2)
+
+            //Console.WriteLine("Main Program done!");
+            //Console.ReadKey();
+
+            #endregion
+
+            //DATA SHARING AND SYNCHRONIZATION
+
+            var tasks = new List<Task>();
+            var ba = new BankAccount();
+            for(int i = 0; i < 10; i++)
             {
-                throw new InvalidOperationException("Can't do this!") { Source = "t"};
-            });
-            var t2 = Task.Factory.StartNew(() =>
-            {
-                throw new AccessViolationException("Can't access this!") { Source = "t2" };
-            });
-            try
-            {
-                Task.WaitAll(t, t2);
-            }
-            catch (AggregateException ae)
-            {
-                //foreach(var a in ae.InnerExceptions)
-                //{
-                //    Console.WriteLine($"Exception {a.GetType()} from {a.Source}");
-                //}
-                ae.Handle(e =>
+                tasks.Add(Task.Factory.StartNew(() =>
                 {
-                    if (e is InvalidOperationException)
+                    for (int j = 0; j < 1000; j++)
                     {
-                        Console.WriteLine("Invalid op!");
-                        return true;
+                        ba.Deposit(100);
                     }
-                    else return false;
-                });
+                }));
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.WithDraw(100);
+                    }
+                }));
+                
             }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"Final balance is {ba.Balance}");
 
-            Console.WriteLine("Main Program done!");
-            Console.ReadKey();
 
         }
-
-
+        #region TASK CLASS
         public static int TextLength(object o)
         {
             Console.WriteLine($"\nTask with id {Task.CurrentId} proccessing object {o}");
@@ -164,8 +228,32 @@ namespace ParallelProgramming
                 Console.Write(c);
             }
         }
+        #endregion
 
     }
+
+    public class BankAccount 
+    {
+        public object padLock = new object();
+        public int Balance { get; private set; }
+
+        public void Deposit(int amount)
+        {
+            lock(padLock)
+            {
+                Balance += amount;
+            }
+        }
+        public void WithDraw(int amount)
+        {
+            lock (padLock)
+            {
+                Balance -= amount;
+            }
+        }
+
+    }
+
 }
 
 
