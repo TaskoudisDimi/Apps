33aa6b876fb816489480561b057f69f91ad2599a
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-07-04 10:49:22 +0300

    End of Parallel LINQ, Start Async Await

--- ParallelProgramming/Program.cs
+++ ParallelProgramming/Program.cs
@@ -1,13 +1,14 @@
 ﻿
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Microsoft.Diagnostics.Runtime.Utilities;
 using System.Collections.Concurrent;
 
 namespace ParallelProgramming
 {
     public class Program
     {
-        
+
         public static void Main(string[] args)
         {
             #region TASK CLASS
@@ -730,9 +731,104 @@ namespace ParallelProgramming
 
             #region AsParallel and ParallelQuery
 
+            //PLINQ is parallel LINQ
+            //const int count = 50;
+            //var items = Enumerable.Range(1, count).ToArray();
+            //var results = new int[count];
+            //items.AsParallel().ForAll(x =>
+            //{
+            //    int newValue = x * x * x;
+            //    Console.WriteLine($"{newValue} ({Task.CurrentId})\t");
+            //    results[x - 1] = newValue;
+
+            //});
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //foreach (var i in results)
+            //{
+            //    Console.WriteLine($"{i}\t");
+            //}
+            //Console.WriteLine();
+
+            //var cubes = items.AsParallel().AsOrdered().Select(x => x * x * x);
+            //foreach (var i in cubes)
+            //{
+            //    Console.WriteLine($"{i}\t");
+            //}
+            //Console.WriteLine();
+
+            #endregion
+
+            #region Cancellation and Exception
+
+            //var cts = new CancellationTokenSource();
+
+            //var items = ParallelEnumerable.Range(1, 20);
+            //var results = items.WithCancellation(cts.Token).Select(i =>
+            //{
+
+            //    double result = Math.Log10(i);
+            //    Console.WriteLine($"i = {i}, tid = {Task.CurrentId}");
+            //    return result;
+            //});
+            //try
+            //{
+            //    foreach (var item in results)
+            //    {
+            //        if (item > 1)
+            //            cts.Cancel();
+            //        Console.WriteLine($"result = {item}");
+            //    }
+            //}
+            //catch (AggregateException ae)
+            //{
+            //    ae.Handle(e =>
+            //    {
+            //        Console.WriteLine($"{e.GetType().Name}: {e.Message}");
+            //        return true;
+            //    });
+            //}
+            #endregion
+
+            #region Merge Options
+
+            //var numbers = Enumerable.Range(1,20).ToArray();
+            //var results = numbers.AsParallel().WithMergeOptions(ParallelMergeOptions.NotBuffered).Select(x =>
+            //{
+            //    var result = Math.Log10(x);
+            //    Console.WriteLine($"Produced {result}");
+            //    return result;
+            //});
+            //foreach(var item in results)
+            //{
+            //    Console.WriteLine($"Consumed {item}");
+            //}
+
+            #endregion
+
+
+            #region Custom Aggregation
+
+            //var sum = Enumerable.Range(1, 1000).Sum();
+            //var sum = Enumerable.Range(1,1000).Aggregate(0, (i, acc) => i + acc);
+
+            //var sum = ParallelEnumerable.Range(1,1000).Aggregate(0, (partialSum,i) => partialSum += i,
+            //    (total, subtotal) => total += subtotal, i=> i);
+
+            //Console.WriteLine($"Sum = {sum}");
+
             #endregion
 
             #endregion
+
+            #region Asynchronous Programming (Async/Await)
+
+
+
+            #endregion
+
+
 
         }
         //static ReaderWriterLockSlim padlock = new ReaderWriterLockSlim();
@@ -952,7 +1048,7 @@ namespace ParallelProgramming
 
         #region Parallel LINQ
 
-        
+
 
 
         #endregion
